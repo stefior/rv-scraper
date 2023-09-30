8290940954d3a0dacc3736de5aec778b970a610d
stefior <stephencfiorenza@gmail.com>
2023-09-30 12:08:38 -0400

    feat(feetInchesToInches): allow parsing measurements with trailing text

--- src/helpers/feetInchesToInches.js
+++ src/helpers/feetInchesToInches.js
@@ -1,7 +1,7 @@
 /**
  * Converts a string representing a measurement in feet and inches to just inches.
  *
- * @param {string} feetInches - The measurement to convert, formatted as a variation of X\' Y".
+ * @param {string} feetInches - The measurement to convert, formatted as a variation of X' Y" with optional text following.
  *
  * @throws Will throw an error if the input format is invalid.
  *
@@ -21,11 +21,14 @@
  * feetInchesToInches("8 ft.")
  * // returns 96
  *
- * This function is useful for when database's field only takes inches (e.g. Width).
+ * feetInchesToInches("5' 1\" w/A/C")
+ * // returns 61
+ *
+ * This function is useful for when a database's field only takes inches (e.g. Width).
  */
 export default function feetInchesToInches(feetInches) {
   const pattern =
-    /^(\d+\.?\d*)\s*(?:'|ft|ft\.)\s*(?:(\d+\.?\d*)\s*(?:''|"|in|in\.)?)?$/i;
+    /^(\d+\.?\d*)\s*(?:'|ft|ft\.)\s*(?:(\d+\.?\d*)\s*(?:''|"|in|in\.)?)?/i;
   const matches = feetInches.match(pattern);
   if (!matches) throw new Error("Invalid input format");
 
