d8bc0d71fb3384dbb1891b102776086928919c7a
stefior <stephencfiorenza@gmail.com>
2023-09-30 13:29:53 -0400

    feat: enable link extractor to work with an array of sites

--- src/helpers/extractAllSelectedLinks.js
+++ src/helpers/extractAllSelectedLinks.js
@@ -1,20 +1,35 @@
 import puppeteer from "puppeteer";
 
-// TODO: add functionality for input array of many urls or array of one
-export default async function extractAllSelectorLinks(url, selector) {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-  await page.goto(url);
+export default async function extractAllSelectorLinks(urls, selector) {
+  if (!Array.isArray(urls)) {
+    throw console.error("Invalid input: urls should be an array");
+  }
+  if (typeof selector !== 'string') {
+    throw console.error("Invalid input: selector should be a string");
+  }
 
-  await page.waitForSelector(selector);
-  const links = await page.$$eval(selector, (allAs) => {
-    const arr = [];
-    for (a of allAs) {
-      arr.push(a.href);
-    }
-    return arr;
-  });
+  const browser = await puppeteer.launch({ headless: "new" });
+  let allLinks = [];
+  let count = 0
+  for (let url of urls) {
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.waitForSelector(selector);
+    const links = await page.$$eval(selector, (allAs) => {
+      const arr = [];
+      for (a of allAs) {
+        arr.push(a.href);
+      }
+      return arr;
+    });
+    allLinks.push(...links);
+    
+    count++
+    console.log(`urls completed: ${count} of ${urls.length}`)
+    page.close();
+  }
 
   await browser.close();
-  return links;
+  return allLinks;
 }
