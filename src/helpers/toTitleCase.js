1073dd1da5fc8399b7984438c07db9a87cce4485
stefior <stephencfiorenza@gmail.com>
2023-10-02 09:15:38 -0400

    chore: replace deprecated function with the new version

--- src/helpers/toTitleCase.js
+++ src/helpers/toTitleCase.js
@@ -9,6 +9,6 @@
  */
 export default function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
 }
